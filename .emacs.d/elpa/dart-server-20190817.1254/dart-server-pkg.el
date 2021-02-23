b721211b8e94ee27c98794b43389d428003ab9a4
Luca Schwan <schwan.luc@gmail.com>
2021-02-23 01:23:41 +0100

    inital commit

---
+++ .emacs.d/elpa/dart-server-20190817.1254/dart-server-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from /Users/Luca_Schwan/.emacs.d/elpa/dart-server-20190817.1254/dart-server.el  -*- no-byte-compile: t -*-
+(define-package "dart-server" "20190817.1254" "Minor mode for editing Dart files" '((emacs "24.5") (cl-lib "0.5") (dash "2.10.0") (flycheck "0.23") (s "1.10")) :commit "aba838e8ee2f30309f366e8a91c17616549003ce" :authors '(("Natalie Weizenbaum") ("Brady Trainor" . "mail@bradyt.com")) :maintainer '("Brady Trainor" . "mail@bradyt.com") :keywords '("languages") :url "https://github.com/bradyt/dart-server")
---
+++ .emacs.d/var/bookmark-default.el
@@ -0,0 +1,10 @@
+;;;; Emacs Bookmark Format Version 1 ;;;; -*- coding: utf-8-emacs -*-
+;;; This format is meant to be slightly human-readable;
+;;; nevertheless, you probably don't want to edit it.
+;;; -*- End Of Bookmark File Format Version Stamp -*-
+(("org-capture-last-stored"
+ (filename . "~/Dropbox/org-files/brain.org")
+ (front-context-string . "** TODO fix org-")
+ (rear-context-string . "-16 Sat 16:15]\n\n")
+ (position . 191))
+)
