5ecd4441562d8d7156e8e178690a70b4ce2ed50c
LucaSchwan <schwan.luc@gmail.com>
2021-05-15 18:32:58 +0200

    updated qtile

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -232,7 +232,6 @@ focus_on_window_activation = "smart"
 def start_once():
     home = os.path.expanduser('~')
     subprocess.call([home + '/.config/qtile/autostart.sh']) 
-    lazy.restart()
 
 
 # XXX: Gasp! We're lying here. In fact, nobody really uses or cares about this
