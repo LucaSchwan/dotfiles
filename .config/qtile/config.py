544d6d66261bb4521aecdb263636c078c33ffe4a
ehrenschwan <luca@ehrenschwan.com>
2021-05-19 23:01:17 +0200

    start on two monitor layout

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -63,7 +63,10 @@ keys = [
 
     # flip layout
     Key([mod, "shift"], "space", lazy.layout.flip()),
-    
+
+    # screen keybinds
+    Key([mod], "z", lazy.prev_screen(),
+        desc='Move focus to prev monitor'),
 
     # terminal and editor keybinds 
     Key([mod], "Return", lazy.spawn(terminal),
@@ -152,91 +155,112 @@ widget_defaults = dict(
     background=colors[2]
 )
 
-extension_defaults = widget_defaults.copy()
-screens = [
-    Screen(
-        top=bar.Bar(
-            [
-                widget.GroupBox(
-                    margin_y = 4,
-                    margin_x = 0,
-                    padding_y = 4,
-                    padding_x = 3,
-                    borderwidth = 2,
-                    active = colors[2],
-                    inactive = colors[2],
-                    rounded = False,
-                    highlight_color = colors[1],
-                    highlight_method = "line",
-                    this_current_screen_border = colors[3],
-                    this_screen_border = colors [4],
-                    other_current_screen_border = colors[0],
-                    other_screen_border = colors[0],
-                    foreground = colors[2],
-                    background = colors[0]
-                    
+def init_widgets_list():
+    widgets_list = [
+        widget.GroupBox(
+            margin_y = 4,
+            margin_x = 0,
+            padding_y = 4,
+            padding_x = 3,
+            borderwidth = 2,
+            active = colors[2],
+            inactive = colors[2],
+            rounded = False,
+            highlight_color = colors[1],
+            highlight_method = "line",
+            this_current_screen_border = colors[3],
+            this_screen_border = colors [4],
+            other_current_screen_border = colors[0],
+            other_screen_border = colors[0],
+            foreground = colors[2],
+            background = colors[0]
+            
                 ),
-                widget.Sep(
-                    linewidth = 0,
-                    padding = 40,
-                    foreground = colors[2],
-                    background = colors[0]
-                ),
-                widget.WindowName(
-                    foreground = colors[2],
-                    background = colors[0]
-                ),
-                widget.Sep(
-                    linewidth = 0,
-                    padding = 40,
-                    foreground = colors[2],
-                    background = colors[0]
-                ),
-                widget.CurrentLayout(
-                    foreground = colors[2],
-                    background = colors[0]
-                ),
-                widget.CurrentLayoutIcon(
-                    foreground = colors[2],
-                    background = colors[0],
-                    scale = 0.5
-                ),
-                widget.Systray(
-                    foreground = colors[2],
-                    background = colors[0]
-                ),
-                widget.Sep(
-                    linewidth = 0,
-                    padding = 20,
-                    foreground = colors[2],
-                    background = colors[0]
-                ),
-                widget.Clock(
-                    margin_y = 3,
-                    margin_x = 0,
-                    padding_y = 5,
-                    padding_x = 3,
-                    format = '%a %d/%m/%y  %H:%M:%S',
-                    foreground = colors[2],
-                    background = colors[0]
-                ),
-                widget.Sep(
-                    linewidth = 0,
-                    padding = 20,
-                    foreground = colors[2],
-                    background = colors[0]
-                ), 
-                widget.QuickExit(
-                    foreground = colors[2],
-                    background = colors[0],
-                    default_text = 'kill 💀',
-                    countdown_format = '[{}s]   '
-                ),
-            ],
-            24,
+        widget.Sep(
+            linewidth = 0,
+            padding = 40,
+            foreground = colors[2],
+            background = colors[0]
         ),
-    ),
-]
+        widget.WindowName(
+            foreground = colors[2],
+            background = colors[0]
+        ),
+        widget.Sep(
+            linewidth = 0,
+            padding = 40,
+            foreground = colors[2],
+            background = colors[0]
+        ),
+        widget.CurrentLayout(
+            foreground = colors[2],
+            background = colors[0]
+        ),
+        widget.CurrentLayoutIcon(
+            foreground = colors[2],
+            background = colors[0],
+            scale = 0.5
+        ),
+        widget.Systray(
+            foreground = colors[2],
+            background = colors[0]
+        ),
+        widget.KeyboardLayout(
+            configured_keyboards = ['us colemak'],
+            display_map = {'us colemak': ''},
+            foreground = colors[2],
+            background = colors[0]
+        ),
+        widget.Sep(
+            linewidth = 0,
+            padding = 20,
+            foreground = colors[2],
+            background = colors[0]
+        ),
+        widget.Clock(
+            margin_y = 3,
+            margin_x = 0,
+            padding_y = 5,
+            padding_x = 3,
+            format = '%a %d/%m/%y  %H:%M:%S',
+            foreground = colors[2],
+            background = colors[0]
+        ),
+        widget.Sep(
+            linewidth = 0,
+            padding = 20,
+            foreground = colors[2],
+            background = colors[0]
+        ), 
+        widget.QuickExit(
+            foreground = colors[2],
+            background = colors[0],
+            default_text = 'kill 💀',
+            countdown_format = '[{}s]   '
+        ),
+    ]
+    return widgets_list
+
+def init_widgets_screen1():
+    widgets_screen1 = init_widgets_list()
+    del widgets_screen1[7]               # Slicing removes unwanted widgets (systray) on Monitors 1,3
+    return widgets_screen1
+
+def init_widgets_screen2():
+    widgets_screen2 = init_widgets_list()
+    return widgets_screen2                 # Monitor 2 will display all widgets in widgets_list
+
+def init_screens():
+    return [Screen(top=bar.Bar(widgets=init_widgets_screen1(), opacity=1.0, size=20)),
+            Screen(top=bar.Bar(widgets=init_widgets_screen2(), opacity=1.0, size=20))]
+
+if __name__ in ["config", "__main__"]:
+    screens = init_screens()
+    widgets_list = init_widgets_list()
+    widgets_screen1 = init_widgets_screen1()
+    widgets_screen2 = init_widgets_screen2()
+
+
 
 # Drag floating layouts.
 mouse = [
--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -18,3 +18,16 @@
 
 ;;load configuration file
 (org-babel-load-file "~/.emacs.d/config.org")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(evil-colemak-basics xref-js2 which-key visual-fill-column use-package typescript-mode treepy rainbow-delimiters org-gcal org-bullets no-littering magit lsp-ui lsp-ivy lsp-haskell lsp-dart js2-refactor ivy-rich helpful general evil-nerd-commenter evil-collection doom-themes doom-modeline dart-server counsel-projectile company-box command-log-mode all-the-icons-dired)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
