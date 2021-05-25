7c1008b4a76620fa256cec96d7ec6c1f89e0ab56
ehrenschwan <luca@ehrenschwan.com>
2021-05-25 15:33:15 +0200

    correction in config list

--- .config/rofi/scripts/configs.sh
+++ .config/rofi/scripts/configs.sh
@@ -5,13 +5,14 @@ EDITOR="emacsclient -c -a 'emacs'"
 declare -a options=(
     "alacritty - $HOME/.config/alacritty/alacritty.yml"
     "emacs - $HOME/.emacs.d/config.org"
+    "picom - $HOME/.config/picom/picom.conf"
     "qtile - $HOME/.config/qtile/config.py"
-    "Xresources - $HOME/.Xrecouces"
+    "Xresources - $HOME/.Xresources"
     "zsh - $HOME/.zshrc"
     "quit"
 )
 
-choice=$(printf '%s\n' "${options[@]}" | rofi -dmenu -r "edit config:" -font 'Fira Code 12')
+choice=$(printf '%s\n' "${options[@]}" | rofi -dmenu -p "edit config" -font 'Fira Code 12')
 
 if [[ "$choice" == "quit" ]]; then
     echo "program terminated" && exit 1
