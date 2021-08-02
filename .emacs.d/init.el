72274a49f93d18cb024be617376391fc46d3595b
ehrenschwan <luca@ehrenschwan.com>
2021-08-03 01:59:58 +0200

    keybindings

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -6,8 +6,6 @@
                          ("elpa" . "https://elpa.gnu.org/packages/")))
 
 (package-initialize)
-(unless package-archive-contents
-  (package-refresh-contents))
 
 ;; Initialize use-package on non-Linux platforms
 (unless (package-installed-p 'use-package)
@@ -15,6 +13,9 @@
 
 (require 'use-package)
 (setq use-package-always-ensure t)
+
+(unless package-archive-contents
+  (package-refresh-contents))
 
 ;;load configuration file
 (org-babel-load-file "~/.emacs.d/config.org")
@@ -24,7 +25,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(evil-colemak-basics xref-js2 which-key visual-fill-column use-package typescript-mode treepy rainbow-delimiters org-gcal org-bullets no-littering magit lsp-ui lsp-ivy lsp-haskell lsp-dart js2-refactor ivy-rich helpful general evil-nerd-commenter evil-collection doom-themes doom-modeline dart-server counsel-projectile company-box command-log-mode all-the-icons-dired)))
+   '(restart-emacs auctex org-tree-slide undo-tree evil-colemak-basics xref-js2 which-key visual-fill-column use-package typescript-mode treepy rainbow-delimiters org-gcal org-bullets no-littering magit lsp-ui lsp-ivy lsp-haskell lsp-dart js2-refactor ivy-rich helpful general evil-nerd-commenter evil-collection doom-themes doom-modeline dart-server counsel-projectile company-box command-log-mode all-the-icons-dired)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
