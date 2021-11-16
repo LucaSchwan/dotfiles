2df3569e89a02c0310d914c66b048afbaba417fd
ehrenschwan <luca@ehrenschwan.com>
2021-11-16 22:33:21 +0100

    added which key idle

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -29,8 +29,10 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(ember-keymap-prefix ".")
+ '(indent-tabs-mode nil)
  '(package-selected-packages
-   '(exec-path-from-shell ember-mode web-mode rjsx-mode restart-emacs auctex org-tree-slide undo-tree evil-colemak-basics xref-js2 which-key visual-fill-column use-package typescript-mode treepy rainbow-delimiters org-gcal org-bullets no-littering magit lsp-ui lsp-ivy lsp-haskell lsp-dart js2-refactor ivy-rich helpful general evil-nerd-commenter evil-collection doom-themes doom-modeline dart-server counsel-projectile company-box command-log-mode all-the-icons-dired)))
+   '(exec-path-from-shell ember-mode web-mode rjsx-mode restart-emacs auctex org-tree-slide undo-tree evil-colemak-basics xref-js2 which-key visual-fill-column use-package typescript-mode treepy rainbow-delimiters org-gcal org-bullets no-littering magit lsp-ui lsp-ivy lsp-haskell lsp-dart js2-refactor ivy-rich helpful general evil-nerd-commenter evil-collection doom-themes doom-modeline dart-server counsel-projectile company-box command-log-mode all-the-icons-dired))
+ '(tab-width 4))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
