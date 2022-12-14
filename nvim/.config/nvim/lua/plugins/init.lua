7811ded8bcda448aaaad80821c23f93098decc09
Luca Schwan <schwan.luc@gmail.com>
2022-12-14 12:17:18 +0100

    formatting in plugins and added auto pairs

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -1,28 +1,35 @@
-return require('packer').startup(function()
-  -- Packer can manage itself
-  use { 'wbthomason/packer.nvim' }
+return require("packer").startup(function()
+	-- Packer can manage itself
+	use({ "wbthomason/packer.nvim" })
 
-  use { "ellisonleao/gruvbox.nvim" }
+	use({ "ellisonleao/gruvbox.nvim" })
 
-  use { 'neovim/nvim-lspconfig' }
-  use { 'jose-elias-alvarez/null-ls.nvim'}
+	use({ "neovim/nvim-lspconfig" })
+	use({ "jose-elias-alvarez/null-ls.nvim" })
 
-  use {
-    'nvim-telescope/telescope.nvim', tag = '0.1.0',
-    requires = { {'nvim-lua/plenary.nvim'} }
-  }
+	use({
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.0",
+	})
+	use({ "nvim-lua/plenary.nvim" })
 
-  -- Treesitter
-  use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
-  use { 'nvim-treesitter/nvim-treesitter-refactor' }
+	-- Treesitter
+	use({ "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" })
+	use({ "nvim-treesitter/nvim-treesitter-refactor" })
 
-  -- nvim-cmp
-  use { 'hrsh7th/cmp-nvim-lsp' }
-  use { 'hrsh7th/cmp-buffer' }
-  use { 'hrsh7th/cmp-path' }
-  use { 'hrsh7th/cmp-cmdline' }
-  use { 'hrsh7th/nvim-cmp' }
+	-- nvim-cmp
+	use({ "hrsh7th/cmp-nvim-lsp" })
+	use({ "hrsh7th/cmp-buffer" })
+	use({ "hrsh7th/cmp-path" })
+	use({ "hrsh7th/cmp-cmdline" })
+	use({ "hrsh7th/nvim-cmp" })
 
-  use { 'kdheepak/lazygit.nvim' }
+	use({
+		"windwp/nvim-autopairs",
+		config = function()
+			require("nvim-autopairs").setup({})
+		end,
+	})
 
+	use({ "kdheepak/lazygit.nvim" })
 end)
